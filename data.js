55b211f46fc2f07e3c86497744c7f901feff5262
Yash <ymohta21@gmail.com>
2023-10-23 14:17:33 +0530

    StackOverFlow  icon added

--- data.js
+++ data.js
@@ -208,6 +208,22 @@ const dataset = [
     ],
   },
   {
+    title: "StackOverFlow",
+    slug: "stack_overflow",
+    addedBy: "Yash-Mohta",
+    cardImagePath: "./RandomSize/stack_overflow.png",
+    sizes: [
+      { title: "16×16", folderPath: "./16/", format: "png" },
+      { title: "24×24", folderPath: "./24/", format: "png" },
+      { title: "32×32", folderPath: "./32/", format: "png" },
+      { title: "48×48", folderPath: "./48/", format: "png" },
+      { title: "64×64", folderPath: "./64/", format: "png" },
+      { title: "96×96", folderPath: "./96/", format: "png" },
+      { title: "128×128", folderPath: "./128/", format: "png" },
+      { title: "Random Size", folderPath: "./RandomSize/", format: "png" },
+    ],
+  },
+  {
     title: "Topcoder",
     slug: "topcoder",
     addedBy: "Shariar-Hasan",
