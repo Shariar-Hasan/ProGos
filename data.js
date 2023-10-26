f99e5f93129111d6a568720751baaffa534bfba6
gaurovgiri <gaurovgiri@gmail.com>
2023-10-27 01:16:54 +0545

    feat: adding logo of w3schools of 16,24,32,48,64,96,12,512 square pixels

--- data.js
+++ data.js
@@ -255,4 +255,20 @@ const dataset = [
       { title: "Random Size", folderPath: "./RandomSize/", format: "png" },
     ],
   },
+  {
+    title: "W3Schools",
+    slug: "w3schools",
+    addedBy: "Gaurovgiri",
+    cardImagePath: "./RandomSize/w3schools.png",
+    sizes: [
+      { title: "16×16", folderPath: "./16/", format: "png" },
+      { title: "24×24", folderPath: "./24/", format: "png" },
+      { title: "32×32", folderPath: "./32/", format: "png" },
+      { title: "48×48", folderPath: "./48/", format: "png" },
+      { title: "64×64", folderPath: "./64/", format: "png" },
+      { title: "96×96", folderPath: "./96/", format: "png" },
+      { title: "128×128", folderPath: "./128/", format: "png" },
+      { title: "Random Size", folderPath: "./RandomSize/", format: "png" },
+    ],
+  },
 ];
