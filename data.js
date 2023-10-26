08029436ae3496e5715387e4f01febe4426cf6b3
gaurovgiri <gaurovgiri@gmail.com>
2023-10-26 19:36:18 +0545

    feat: adding logo of sololearn of 16,24,32,48,64,96,128,512 square pixels

--- data.js
+++ data.js
@@ -192,6 +192,22 @@ const dataset = [
     ],
   },
   {
+    title: "Sololearn",
+    slug: "sololearn",
+    addedBy: "Gaurovgiri",
+    cardImagePath: "./RandomSize/sololearn.png",
+    sizes: [
+      { title: "16×16", folderPath: "./16/", format: "png" },
+      { title: "24×24", folderPath: "./24/", format: "png" },
+      { title: "32×32", folderPath: "./32/", format: "png" },
+      { title: "48×48", folderPath: "./48/", format: "png" },
+      { title: "64×64", folderPath: "./64/", format: "png" },
+      { title: "96×96", folderPath: "./96/", format: "png" },
+      { title: "128×128", folderPath: "./128/", format: "png" },
+      { title: "Random Size", folderPath: "./RandomSize/", format: "png" },
+    ],
+  },
+  {
     title: "SPOJ",
     slug: "spoj",
     addedBy: "Shariar-Hasan",
