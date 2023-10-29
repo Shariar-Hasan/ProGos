5cc4a704c214d0e7e21f987293f09d06cf9c48ec
gaurovgiri <gaurovgiri@gmail.com>
2023-10-29 22:42:59 +0545

    feat: adding logo of Exercism of 16,24,32,48,64,96,128 square pixels

--- data.js
+++ data.js
@@ -112,6 +112,22 @@ const dataset = [
     ],
   },
   {
+    title: "Exercism",
+    slug: "exercism",
+    addedBy: "gaurovgiri",
+    cardImagePath: "./RandomSize/exercism.png",
+    sizes: [
+      { title: "16×16", folderPath: "./16/", format: "png" },
+      { title: "24×24", folderPath: "./24/", format: "png" },
+      { title: "32×32", folderPath: "./32/", format: "png" },
+      { title: "48×48", folderPath: "./48/", format: "png" },
+      { title: "64×64", folderPath: "./64/", format: "png" },
+      { title: "96×96", folderPath: "./96/", format: "png" },
+      { title: "128×128", folderPath: "./128/", format: "png" },
+      { title: "Random Size", folderPath: "./RandomSize/", format: "png" },
+    ],
+  },
+  {
     title: "FreeCodeCamp",
     slug: "freeCodeCamp",
     addedBy: "Shariar-Hasan",
